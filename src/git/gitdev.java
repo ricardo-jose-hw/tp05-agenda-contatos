876260d47d3d8ddf8174c1abc5a050ce60118fe8
Ricardos Jose <rcrdo.dev@gmail.com>
2026-09-01 21:52:36 -0300

    B- Menu de Opções

--- src/git/gitdev.java
+++ src/git/gitdev.java
@@ -1,5 +1,25 @@
 package git;
 
 public class gitdev {
+static void exibirMenu(){
+    System.out.print("=================================");
+    System.out.print("MENU DE CONTATOS");
+    System.out.print("=================================");
+    System.out.println("=================================");
+
+    System.out.println("1 - CADASTRAR Contato");
+    System.out.println("2 - LISTAR Contatos");
+    System.out.println("3 - BUSCAR Contatos");
+    System.out.println("4 - EXCLUIR  Contatos");
+    System.out.println("5 - SAIR");
+
+
+
+}
+
+    public static void main(String[] args) {
+exibirMenu();
+}
+
 
 }
